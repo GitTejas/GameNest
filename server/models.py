84ae903c1c5a11a4876751765fae81844546beb9
Tejas Nampalli <TSNampalli@gmail.com>
2024-11-07 13:21:38 -0500

    Project Start

--- server/models.py
+++ server/models.py
@@ -4,3 +4,20 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class Game(db.Model, SerializerMixin):
+    pass
+
+
+
+
+
+class Store(db.Model, SerializerMixin):
+    pass
+
+
+
+
+
+
+class Listing(db.Model, SerializerMixin):
+    pass
